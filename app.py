eba85378f18c4e8542eb27dc2794d8f827cffb07
pzpz <idleversecat@gmail.com>
2023-03-28 14:04:49 +0900

    deleted:    .vscode/settings.json modified:   app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if ':hikocheck: < サーバーたてました！' in message.content:
+    if 'サーバーたてました！' in message.content:
         await message.add_reaction('<:hk:1089860585396641843>')
         await message.add_reaction('<:hkc:1089860583983169596>')
         await message.add_reaction('✅')
-    if ':hikozzz: サーバーとめました！' in message.content:
+    if 'サーバーとめました！' in message.content:
         await message.add_reaction('<:hk:1089860585396641843>')
         await message.add_reaction('<:hkz:1089860581223305317>')
         await message.add_reaction('💤')
